46b438161d3d7b6ec52e9c2e4df5ab3b3ba84ebd
Nikolina Marinić <61114828+Nina621@users.noreply.github.com>
2020-11-12 14:43:38 +0100

    Add files via upload

---
+++ Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LeaveManager.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LeaveManagment.Areas.Identity.IdentityHostingStartup))]
+namespace LeaveManagment.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
